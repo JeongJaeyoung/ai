5228149827cacee4c4aa99ec797d4b2334d353bf
JaeyoungJeong <jjy9678@naver.com>
2020-03-13 22:56:27 +0900

    file

---
+++ static/js/p3-ch2-s2-ai.js
@@ -0,0 +1,64 @@
+// toggle 버튼 설정
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step1.
+$(function () {
+    $("#button-linear-step1-matrix").on("click", function () {
+        $("#div-linear-step1-matrix").toggleClass("disp-none");
+    });
+});
+
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step2.proof
+$(function () {
+    $("#button-linear-step2-matrix").on("click", function () {
+        $("#div-linear-step2-matrix").toggleClass("disp-none");
+    });
+});
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step3.proof
+$(function () {
+    $("#button-linear-step3-proof").on("click", function () {
+        $("#div-linear-step3-proof").toggleClass("disp-none");
+    });
+});
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step3.matrix
+$(function () {
+    $("#button-linear-step3-matrix").on("click", function () {
+        $("#div-linear-step3-matrix").toggleClass("disp-none");
+    });
+});
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-linear_regression-step3.range
+$(function () {
+    $("#button-linear-step3-range").on("click", function () {
+        $("#div-linear-step3-range").toggleClass("disp-none");
+    });
+});
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-logistic-step1-matrix
+$(function () {
+    $("#button-logistic-step1-matrix").on("click", function () {
+        $("#div-logistic-step1-matrix").toggleClass("disp-none");
+    });
+});
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-logistic-step2-matrix
+$(function () {
+    $("#button-logistic-step2-matrix").on("click", function () {
+        $("#div-logistic-step2-matrix").toggleClass("disp-none");
+    });
+});
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-logistic-step3-proof
+$(function () {
+    $("#button-logistic-step3-proof").on("click", function () {
+        $("#div-logistic-step3-proof").toggleClass("disp-none");
+    });
+});
+
+// part3(computer)-chapter2(reality)-section2(ai)-supervised_learning-neural-step3-range
+$(function () {
+    $("#button-neural-step3-range").on("click", function () {
+        $("#div-neural-step3-range").toggleClass("disp-none");
+    });
+});
